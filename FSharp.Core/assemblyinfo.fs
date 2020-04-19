f7aa5431de01f8a9f3e654f01df1367eeffcbaa1
Player <atiladojanga@hotmail.com>
2020-04-19 06:19:37 -0300

    Fix assemblyinfo.fs

--- FSharp.Core/assemblyinfo.fs
+++ FSharp.Core/assemblyinfo.fs
@@ -3,6 +3,6 @@
 namespace Microsoft.FSharp
 open System.Reflection
 
-[<assembly:AssemblyInformationalVersion("4.5.0.0.  Commit Hash: 26c3228402cadbfe92aa13c6bb0c323830767754.")>]
+[<assembly:AssemblyInformationalVersion("4.5.0.0.  Commit Hash: f2e785baaa47587b1e4f1d6cd66b73a985ff1d15.")>]
 do()
 
