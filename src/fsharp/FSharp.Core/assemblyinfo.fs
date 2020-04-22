249552feffd3b142c781d7f1f0e9104bcfa744ba
Player <atiladojanga@hotmail.com>
2020-04-22 13:45:12 -0300

    Fix assemblyinfo.fs

--- src/fsharp/FSharp.Core/assemblyinfo.fs
+++ src/fsharp/FSharp.Core/assemblyinfo.fs
@@ -4,6 +4,6 @@ namespace Microsoft.FSharp
 open System.Reflection
 
 [<assembly: System.Reflection.AssemblyFileVersion("4.700.20.10408")>]
-[<assembly: System.Reflection.AssemblyInformationalVersion("4.7.0-beta.20104.8+")>]
+[<assembly: System.Reflection.AssemblyInformationalVersion("4.7.0-beta.20104.8+cff26c431db0cbf9622675cf2ab74f8606deac51")>]
 do()
 
