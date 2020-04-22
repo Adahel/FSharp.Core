68095190245c87e6bc24e90b05097a21591b14ec
Player <atiladojanga@hotmail.com>
2020-04-22 15:30:39 -0300

    Fix © symbol.

--- src/fsharp/FSharp.Core/assemblyinfo.fs
+++ src/fsharp/FSharp.Core/assemblyinfo.fs
@@ -2,6 +2,7 @@
 
 namespace Microsoft.FSharp
 open System.Reflection
+open System.Resources
 
 [<assembly: AssemblyCompany("Microsoft Corporation")>]
 #if DEBUG
@@ -9,12 +10,12 @@ open System.Reflection
 #else
 [<assembly: AssemblyConfiguration("Release")>]
 #endif
-[<assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")>]
+[<assembly: AssemblyCopyright("\x00a9 Microsoft Corporation.  All Rights Reserved.")>]
 [<assembly: AssemblyFileVersion("4.700.20.10408")>]
 [<assembly: AssemblyInformationalVersion("4.7.0-beta.20104.8+bcbd55043735bd66f49494670377a61a75921824")>]
 [<assembly: AssemblyProduct("FSharp.Core")>]
 [<assembly: AssemblyTitle("FSharp.Core")>]
 [<assembly: AssemblyVersion("4.7.0.0")>]
-[<assembly: System.Resources.NeutralResourcesLanguage("en-US")>]
+[<assembly: NeutralResourcesLanguage("en-US")>]
 do()
 
